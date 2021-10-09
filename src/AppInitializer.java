7e353ef2a8321279807d633ba028b6f89dfbc8a3
Gim128 <gimhanpabasara4@gmail.com>
2021-10-09 09:52:34 +0530

    added AppInitializer.java

---
+++ src/AppInitializer.java
@@ -0,0 +1,26 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) UnitD-lk. All rights reserved.
+ *  *  Licensed under the SriLanka License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @author GImhan Pabasara <gimhanpabasara4@gmail.com> (www.gimhanpabasara.tk)
+ * @since 10/9/2021
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
