66abc338b922a79257d6c543c59983fd9c674e65
Gim128 <gimhanpabasara4@gmail.com>
2021-10-09 11:43:20 +0530

    create Item section

---
+++ src/controller/ItemFormController.java
@@ -0,0 +1,18 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) UnitD-lk. All rights reserved.
+ *  *  Licensed under the SriLanka License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+package controller;
+
+/**
+ * @author GImhan Pabasara <gimhanpabasara4@gmail.com> (www.gimhanpabasara.tk)
+ * @since 10/9/2021
+ */
+public class ItemFormController {
+   public void itemName(){
+       System.out.println("itemName");
+   }
+}
---
+++ src/dto/ItemDTO.java
@@ -0,0 +1,15 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) UnitD-lk. All rights reserved.
+ *  *  Licensed under the SriLanka License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+package dto;
+
+/**
+ * @author GImhan Pabasara <gimhanpabasara4@gmail.com> (www.gimhanpabasara.tk)
+ * @since 10/9/2021
+ */
+public class ItemDTO {
+}
